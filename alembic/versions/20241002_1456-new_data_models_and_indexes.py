1ad0acda33b884b8f457021cefb1c28adfad0de6
Sanchit Ram Arvind <sanchitram@gmail.com>
2024-10-02 15:07:29 -0500

    add a migration for new data models and indexes
    - user and version link
    - user and package link
    - import_id, readme for packages
    - nullable columns for versions
    - licenses table, license_id foreign key

---
+++ alembic/versions/20241002_1456-new_data_models_and_indexes.py
@@ -0,0 +1,418 @@
+"""new data models and indexes
+
+Revision ID: d1fca65a53c0
+Revises: 905522c68f8a
+Create Date: 2024-10-02 14:56:06.022022
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = "d1fca65a53c0"
+down_revision: Union[str, None] = "905522c68f8a"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # add the three new tables
+    op.create_table(
+        "licenses",
+        sa.Column("id", sa.UUID(), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("updated_at", sa.DateTime(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_licenses")),
+        sa.UniqueConstraint("name", name=op.f("uq_licenses_name")),
+    )
+    op.create_table(
+        "user_packages",
+        sa.Column("id", sa.UUID(), nullable=False),
+        sa.Column("user_id", sa.UUID(), nullable=False),
+        sa.Column("package_id", sa.UUID(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("updated_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["package_id"],
+            ["packages.id"],
+            name=op.f("fk_user_packages_package_id_packages"),
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"], ["users.id"], name=op.f("fk_user_packages_user_id_users")
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_user_packages")),
+        sa.UniqueConstraint("user_id", "package_id", name="uq_user_package"),
+    )
+    op.create_table(
+        "user_versions",
+        sa.Column("id", sa.UUID(), nullable=False),
+        sa.Column("user_id", sa.UUID(), nullable=False),
+        sa.Column("version_id", sa.UUID(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("updated_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["user_id"], ["users.id"], name=op.f("fk_user_versions_user_id_users")
+        ),
+        sa.ForeignKeyConstraint(
+            ["version_id"],
+            ["versions.id"],
+            name=op.f("fk_user_versions_version_id_versions"),
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_user_versions")),
+        sa.UniqueConstraint("user_id", "version_id", name="uq_user_version"),
+    )
+
+    # rename record_created_at and record_updated_at to created_at and updated_at
+    op.add_column(
+        "dependencies", sa.Column("created_at", sa.DateTime(), nullable=False)
+    )
+    op.add_column(
+        "dependencies", sa.Column("updated_at", sa.DateTime(), nullable=False)
+    )
+    op.drop_column("dependencies", "record_created_at")
+    op.drop_column("dependencies", "record_updated_at")
+    op.add_column(
+        "load_history", sa.Column("created_at", sa.DateTime(), nullable=False)
+    )
+    op.add_column(
+        "load_history", sa.Column("updated_at", sa.DateTime(), nullable=False)
+    )
+    op.drop_column("load_history", "record_created_at")
+    op.drop_column("load_history", "record_updated_at")
+    op.add_column(
+        "package_managers", sa.Column("created_at", sa.DateTime(), nullable=False)
+    )
+    op.add_column(
+        "package_managers", sa.Column("updated_at", sa.DateTime(), nullable=False)
+    )
+    op.drop_column("package_managers", "record_created_at")
+    op.drop_column("package_managers", "record_updated_at")
+    op.add_column(
+        "package_urls", sa.Column("created_at", sa.DateTime(), nullable=False)
+    )
+    op.add_column(
+        "package_urls", sa.Column("updated_at", sa.DateTime(), nullable=False)
+    )
+    op.drop_column("package_urls", "record_created_at")
+    op.drop_column("package_urls", "record_updated_at")
+    op.add_column("packages", sa.Column("import_id", sa.String(), nullable=False))
+    op.add_column("packages", sa.Column("readme", sa.String(), nullable=True))
+    op.add_column("packages", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("packages", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("packages", "record_created_at")
+    op.drop_column("packages", "record_updated_at")
+    op.add_column("url_types", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("url_types", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("url_types", "record_created_at")
+    op.drop_column("url_types", "record_updated_at")
+    op.add_column("urls", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("urls", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("urls", "record_created_at")
+    op.drop_column("urls", "record_updated_at")
+    op.add_column("user_types", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("user_types", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("user_types", "record_created_at")
+    op.drop_column("user_types", "record_updated_at")
+    op.add_column("user_urls", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("user_urls", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("user_urls", "record_created_at")
+    op.drop_column("user_urls", "record_updated_at")
+    op.add_column("users", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("users", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("users", "record_created_at")
+    op.drop_column("users", "record_updated_at")
+    op.add_column("versions", sa.Column("created_at", sa.DateTime(), nullable=False))
+    op.add_column("versions", sa.Column("updated_at", sa.DateTime(), nullable=False))
+    op.drop_column("versions", "record_created_at")
+    op.drop_column("versions", "record_updated_at")
+
+    # add indexes to import_id and name for packages
+    op.create_index(
+        op.f("ix_packages_import_id"), "packages", ["import_id"], unique=False
+    )
+    op.create_index(op.f("ix_packages_name"), "packages", ["name"], unique=False)
+
+    # assert unique constraint on package_manager_id and import_id for packages
+    op.create_unique_constraint(
+        "uq_package_manager_import_id", "packages", ["package_manager_id", "import_id"]
+    )
+
+    # add all the nullable fields to versions
+    op.add_column("versions", sa.Column("size", sa.Integer(), nullable=True))
+    op.add_column("versions", sa.Column("published_at", sa.DateTime(), nullable=True))
+    op.add_column("versions", sa.Column("license_id", sa.UUID(), nullable=True))
+    op.add_column("versions", sa.Column("downloads", sa.Integer(), nullable=True))
+    op.add_column("versions", sa.Column("checksum", sa.String(), nullable=True))
+
+    # add indexes to downloads, published_at, and size for versions
+    op.create_index(
+        op.f("ix_versions_downloads"), "versions", ["downloads"], unique=False
+    )
+    op.create_index(
+        op.f("ix_versions_published_at"), "versions", ["published_at"], unique=False
+    )
+    op.create_index(op.f("ix_versions_size"), "versions", ["size"], unique=False)
+    op.create_index(op.f("ix_versions_version"), "versions", ["version"], unique=False)
+
+    # assert unique constraint on package_id and version for versions
+    op.create_unique_constraint(
+        "uq_package_version", "versions", ["package_id", "version"]
+    )
+    op.create_foreign_key(
+        op.f("fk_versions_license_id_licenses"),
+        "versions",
+        "licenses",
+        ["license_id"],
+        ["id"],
+    )
+
+
+def downgrade() -> None:
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.add_column(
+        "versions",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "versions",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_constraint(
+        op.f("fk_versions_license_id_licenses"), "versions", type_="foreignkey"
+    )
+    op.drop_constraint("uq_package_version", "versions", type_="unique")
+    op.drop_index(op.f("ix_versions_version"), table_name="versions")
+    op.drop_index(op.f("ix_versions_size"), table_name="versions")
+    op.drop_index(op.f("ix_versions_published_at"), table_name="versions")
+    op.drop_index(op.f("ix_versions_downloads"), table_name="versions")
+    op.drop_column("versions", "updated_at")
+    op.drop_column("versions", "created_at")
+    op.drop_column("versions", "checksum")
+    op.drop_column("versions", "downloads")
+    op.drop_column("versions", "license_id")
+    op.drop_column("versions", "published_at")
+    op.drop_column("versions", "size")
+    op.add_column(
+        "users",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("users", "updated_at")
+    op.drop_column("users", "created_at")
+    op.add_column(
+        "user_urls",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "user_urls",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("user_urls", "updated_at")
+    op.drop_column("user_urls", "created_at")
+    op.add_column(
+        "user_types",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "user_types",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("user_types", "updated_at")
+    op.drop_column("user_types", "created_at")
+    op.add_column(
+        "urls",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "urls",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("urls", "updated_at")
+    op.drop_column("urls", "created_at")
+    op.add_column(
+        "url_types",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "url_types",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("url_types", "updated_at")
+    op.drop_column("url_types", "created_at")
+    op.add_column(
+        "packages",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "packages",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_constraint("uq_package_manager_import_id", "packages", type_="unique")
+    op.drop_index(op.f("ix_packages_name"), table_name="packages")
+    op.drop_index(op.f("ix_packages_import_id"), table_name="packages")
+    op.drop_column("packages", "updated_at")
+    op.drop_column("packages", "created_at")
+    op.drop_column("packages", "readme")
+    op.drop_column("packages", "import_id")
+    op.add_column(
+        "package_urls",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "package_urls",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("package_urls", "updated_at")
+    op.drop_column("package_urls", "created_at")
+    op.add_column(
+        "package_managers",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "package_managers",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("package_managers", "updated_at")
+    op.drop_column("package_managers", "created_at")
+    op.add_column(
+        "load_history",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "load_history",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("load_history", "updated_at")
+    op.drop_column("load_history", "created_at")
+    op.add_column(
+        "dependencies",
+        sa.Column(
+            "record_updated_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "dependencies",
+        sa.Column(
+            "record_created_at",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    op.drop_column("dependencies", "updated_at")
+    op.drop_column("dependencies", "created_at")
+    op.drop_table("user_versions")
+    op.drop_table("user_packages")
+    op.drop_table("licenses")
+    # ### end Alembic commands ###
--- src/pipeline/models/__init__.py
+++ src/pipeline/models/__init__.py
@@ -1,15 +1,22 @@
 # __init__.py
+from __future__ import annotations
 from sqlalchemy import (
     Column,
     DateTime,
     ForeignKey,
+    Integer,
     MetaData,
     String,
     UniqueConstraint,
     func,
 )
+
+# from sqlalchemy.orm import Mapped
+# from sqlalchemy.orm import mapped_column
+# from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
+
 
 naming_convention = {
     "ix": "ix_%(column_0_label)s",
@@ -22,24 +29,32 @@ metadata = MetaData(naming_convention=naming_convention)
 Base = declarative_base(metadata=metadata)
 
 
-# TODO: created_at, updated_at are always set to the current timestamp
+# created_at, updated_at are always set to the current timestamp
 # and should always be part of every single table
 
 
 # TODO:
 # - can we store README?
-# - index on name
-# - add import_id, with index
 class Package(Base):
     __tablename__ = "packages"
+    __table_args__ = (
+        UniqueConstraint(
+            "package_manager_id", "import_id", name="uq_package_manager_import_id"
+        ),
+    )
+
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     derived_id = Column(String, nullable=False, unique=True)  # package_manager/name
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, index=True)
     package_manager_id = Column(
         UUID(as_uuid=True), ForeignKey("package_managers.id"), nullable=False
     )
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    # the import_id is from our data source (e.g., crate_id)
+    # it is non-nullable, and we'll default to name if not provided by source
+    import_id = Column(String, nullable=False, index=True)
+    readme = Column(String, nullable=True)  # definitely no indexing here...
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
     def to_dict(self):
         return {
@@ -53,8 +68,8 @@ class PackageManager(Base):
     __tablename__ = "package_managers"
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     name = Column(String, nullable=False, unique=True)
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
 # this is a collection of all the different type of URLs
@@ -62,8 +77,8 @@ class URL(Base):
     __tablename__ = "urls"
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     url = Column(String, nullable=False, unique=True)
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
     def to_dict(self):
         return {"url": self.url}
@@ -73,18 +88,10 @@ class URLType(Base):
     __tablename__ = "url_types"
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     name = Column(String, nullable=False, unique=True)  # repo, homepage, etc
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
-# TODO: we can add the following fields to Version
-# - size (in bytes), definitely available from crates and NPM
-# - created_at (for release dates, definitely available from crates and NPM)
-#   > this is generally unreliable in PyPI
-#   > for pkgx and Homebrew, this should come from GH
-# - license
-# - downloads
-# - checksum / security stuff (definitely for crates and NPM)
 class Version(Base):
     __tablename__ = "versions"
     __table_args__ = (
@@ -92,12 +99,30 @@ class Version(Base):
     )
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     package_id = Column(UUID(as_uuid=True), ForeignKey("packages.id"), nullable=False)
-    version = Column(String, nullable=False)
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    version = Column(String, nullable=False, index=True)
+    # size, published_at, license_id, downloads, checksum
+    # are nullable bc not all sources provide them
+    size = Column(Integer, nullable=True, index=True)
+    published_at = Column(DateTime, nullable=True, index=True)
+    license_id = Column(UUID(as_uuid=True), ForeignKey("licenses.id"), nullable=True)
+    downloads = Column(Integer, nullable=True, index=True)
+    checksum = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
+
+    # package: Mapped["Package"] = relationship()
+    # license: Mapped["License"] = relationship()
 
     def to_dict(self):
         return {"package_id": self.package_id, "version": self.version}
+
+
+class License(Base):
+    __tablename__ = "licenses"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    name = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
 class DependsOn(Base):
@@ -118,8 +143,8 @@ class DependsOn(Base):
     # ideally, these are non-nullable but diff package managers are picky about this
     dependency_type = Column(String, nullable=True)
     semver_range = Column(String, nullable=True)
-    record_created_at = Column(DateTime, nullable=False)
-    record_updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
 
     def to_dict(self):
         return {
@@ -136,8 +161,8 @@ class LoadHistory(Base):
     package_manager_id = Column(
         UUID(as_uuid=True), ForeignKey("package_managers.id"), nullable=False
     )
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
 class PackageURL(Base):
@@ -151,8 +176,8 @@ class PackageURL(Base):
     package_id = Column(UUID(as_uuid=True), ForeignKey("packages.id"), nullable=False)
     url_id = Column(UUID(as_uuid=True), ForeignKey("urls.id"), nullable=False)
     url_type_id = Column(UUID(as_uuid=True), ForeignKey("url_types.id"), nullable=False)
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
     def to_dict(self):
         return {
@@ -166,8 +191,8 @@ class User(Base):
     __tablename__ = "users"
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     username = Column(String, nullable=False, unique=True)
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
     def to_dict(self):
         return {"username": self.username}
@@ -177,8 +202,8 @@ class UserType(Base):
     __tablename__ = "user_types"
     id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
     name = Column(String, nullable=False, unique=True)  # github, gitlab, crates, etc.
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
 class UserURL(Base):
@@ -192,10 +217,29 @@ class UserURL(Base):
     user_type_id = Column(
         UUID(as_uuid=True), ForeignKey("user_types.id"), nullable=False
     )
-    record_created_at = Column(DateTime, nullable=False, default=func.now())
-    record_updated_at = Column(DateTime, nullable=False, default=func.now())
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
 
 
-# TODO: add models for
-# - UserVersion
-# - UserPackage
+class UserVersion(Base):
+    __tablename__ = "user_versions"
+    __table_args__ = (
+        UniqueConstraint("user_id", "version_id", name="uq_user_version"),
+    )
+    id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    version_id = Column(UUID(as_uuid=True), ForeignKey("versions.id"), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
+
+
+class UserPackage(Base):
+    __tablename__ = "user_packages"
+    __table_args__ = (
+        UniqueConstraint("user_id", "package_id", name="uq_user_package"),
+    )
+    id = Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    package_id = Column(UUID(as_uuid=True), ForeignKey("packages.id"), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
+    updated_at = Column(DateTime, nullable=False, default=func.now())
